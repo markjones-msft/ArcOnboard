69d107b8ee426f79ee60fb2be5b34785c9dd4011
Mark Jones <markjon@microsoft.com>
2025-01-28 12:38:29 +0000

    Deploy

---
+++ Call-Onboard-ArcConnectedServer.ps1
@@ -0,0 +1,20 @@
+﻿param (
+    [string]$CsvPath = "C:\Development\ArcOnboard\arcdeployment.csv",
+    [string]$OnboardingScriptPath = "C:\Development\ArcOnboard\onboard-server.ps1"
+)
+
+# Get the current computer name
+$currentComputerName = $env:COMPUTERNAME
+
+# Read the CSV file
+$servers = Import-Csv -Path $CsvPath
+
+# Find the server parameters based on the current computer name
+$server = $servers | Where-Object { $_.ServerName -eq $currentComputerName }
+
+if ($server) {
+    # Call the onboarding script with the parameters from the CSV
+    & $OnboardingScriptPath -SubscriptionId $server.SubscriptionID -ResourceGroup $server.'Resource Group' -Location $server.Location -Tags $server.Tags -TenantId $server.TenantID -ApplicationName $server.'Application Name'
+} else {
+    Write-Host "No matching server found for the current computer name: $currentComputerName" -ForegroundColor Red
+}
---
+++ Onboard-ArcConnectedServer.ps1
@@ -0,0 +1,36 @@
+﻿param (
+    [string]$SubscriptionId,
+    [string]$ResourceGroup,
+    [string]$Location,
+    [string]$Tags,
+    [string]$ServicePrincipalId,
+    [string]$ServicePrincipalClientSecret,
+    [string]$TenantId,
+    [string]$CorrelationId = "Batch 1",
+    [string]$Cloud = "AzureCloud"
+)
+
+$env:SUBSCRIPTION_ID = $SubscriptionId
+$env:RESOURCE_GROUP = $ResourceGroup
+$env:TENANT_ID = $TenantId
+$env:LOCATION = $Location
+$env:AUTH_TYPE = "principal"
+$env:CORRELATION_ID = $CorrelationId
+$env:CLOUD = $Cloud
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor 3072
+
+# Download the installation package
+Invoke-WebRequest -UseBasicParsing -Uri "https://gbl.his.arc.azure.com/azcmagent-windows" -TimeoutSec 30 -OutFile "$env:TEMP\install_windows_azcmagent.ps1" -proxy "https://proxyserver"
+
+# Install the hybrid agent
+& "$env:TEMP\install_windows_azcmagent.ps1" -proxy "https://proxyserver"
+if ($LASTEXITCODE -ne 0) { exit 1 }
+
+# Run connect command
+& "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect --service-principal-id "$ServicePrincipalId" --service-principal-secret "$ServicePrincipalClientSecret" --resource-group "$env:RESOURCE_GROUP" --tenant-id "$env:TENANT_ID" --location "$env:LOCATION" --subscription-id "$env:SUBSCRIPTION_ID" --cloud "$env:CLOUD" --correlation-id "$env:CORRELATION_ID" --tags "$Tags"
+catch {
+    $logBody = @{subscriptionId="$env:SUBSCRIPTION_ID";resourceGroup="$env:RESOURCE_GROUP";tenantId="$env:TENANT_ID";location="$env:LOCATION";correlationId="$env:CORRELATION_ID";authType="$env:AUTH_TYPE";operation="onboarding";messageType=$_.FullyQualifiedErrorId;message="$_"}
+    Invoke-WebRequest -UseBasicParsing -Uri "https://gbl.his.arc.azure.com/log" -Method "PUT" -Body ($logBody | ConvertTo-Json) -proxy "https://proxyserver" | out-null
+    Write-Host -ForegroundColor red $_.Exception
+}
